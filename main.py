0d5650ec7dd1d92fe19f051df12f1e38fbf9eb8b
Moses Yeung <yo_yeung@yahoo.com.hk>
2022-09-09 16:24:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def move():
     dims = arena["dims"]
     state = arena["state"]
     myselfData = state[myselfDomain]
-    next = ['L','R','T'][random.randrange(3)]
+    next = ['L','R','F'][random.randrange(3)]
     logger.info(f'right now x:{myselfData["x"]}, y:{myselfData["y"]}, next:{next}')
     return next
     # TODO add your implementation here to replace the random response
